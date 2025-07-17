05e76621279c12b40e6b983d486c45c45b75e8e7
Jesús <jesus.mb.1995@gmail.com>
2025-07-17 12:00:30 +0200

    gguf reader from memory

--- ggml/include/gguf.h
+++ ggml/include/gguf.h
@@ -78,7 +78,7 @@ extern "C" {
 
     GGML_API struct gguf_context * gguf_init_empty(void);
     GGML_API struct gguf_context * gguf_init_from_file(const char * fname, struct gguf_init_params params);
-    //GGML_API struct gguf_context * gguf_init_from_buffer(..);
+    GGML_API struct gguf_context * gguf_init_from_buffer(const uint8_t * buffer, size_t size, struct gguf_init_params params);
 
     GGML_API void gguf_free(struct gguf_context * ctx);
 
