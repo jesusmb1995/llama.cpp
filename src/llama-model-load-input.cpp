6fcdce28f69045be3535edd7adb990e19f7982ed
Jesús <jesus.mb.1995@gmail.com>
2025-07-18 10:22:27 +0200

    File load

---
+++ src/llama-model-load-input.cpp
@@ -0,0 +1,19 @@
+
+#include "llama-model-load-input.h"
+
+namespace load_input_variant {
+
+const char * identifier(load_input_t& load_input) {
+    if (std::holds_alternative<fname_load_input>(load_input)) {
+        const auto & file_input = std::get<fname_load_input>(load_input);
+        return file_input.fname.c_str();
+    }
+    static const char * buffer_id_str = "buffer";
+    return buffer_id_str;
+}
+
+bool variant_supports_split_load(load_input_t & load_input) {
+    return std::holds_alternative<fname_load_input>(load_input);
+}
+
+}  // namespace load_input_variant
