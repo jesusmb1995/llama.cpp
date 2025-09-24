tetherto/qvac-ext-lib-llama.cpp

[refactor][mbuffer] File load from variant
- Add code to be able to load a gguf file from a variant (memory or disk).
- Some structs simplify how to load a file and keep track of the pointers (which are now in the same struct).

src/llama-model-load-input.cpp
@@ -1,0 +1,23 @@
+#include "llama-model-load-input.h"
+
+namespace load_input_variant {
+
+const char * identifier(load_input_t & load_input) {
+    if (std::holds_alternative<fname_load_input>(load_input)) {
+        const auto & file_input = std::get<fname_load_input>(load_input);
+        return file_input.fname.c_str();
+    }
+    static const char * buffer_id_str = "buffer";
+    return buffer_id_str;
+}
+
+fname_load_input split_name_from_variant(load_input_t & load_input) {
+    auto file_input = std::get<fname_load_input>(load_input);
+    return file_input;
+}
+
+bool variant_supports_split_load(load_input_t & load_input) {
+    return std::holds_alternative<fname_load_input>(load_input);
+}
+
+}  // namespace load_input_variant

src/llama-model-load.cpp
@@ -1,0 +1,30 @@
+#include "llama-model-load.h"
+
+#include <memory>
+#include <stdexcept>
+#include <variant>
+
+#include "llama-model-loader.h"
+
+gguf_file_load::gguf_file_load(struct ggml_context ** ctx, load_input_t load_input) :
+    params({
+        /*.no_alloc = */ true,
+        /*.ctx      = */ ctx,
+    }) {
+    using namespace load_input_variant;
+    if (std::holds_alternative<fname_load_input>(load_input)) {
+        const auto & file_input = std::get<fname_load_input>(load_input);
+        meta.reset(gguf_init_from_file(file_input.fname.c_str(), params));
+        if (!meta) {
+            throw std::runtime_error(format("%s: failed to load model from %s", __func__, file_input.fname.c_str()));
+        }
+        file = std::make_unique<llama_file_disk>(file_input.fname.c_str(), "ro");
+    } else {
+        const auto & buffer_input = std::get<buffer_load_input>(load_input);
+        meta.reset(gguf_init_from_buffer(*buffer_input.streambuf, params));
+        if (!meta) {
+            throw std::runtime_error(format("%s: failed to load model from buffer", __func__));
+        }
+        file = std::make_unique<llama_file_buffer_ro>(std::move(buffer_input.streambuf));
+    }
+}

jesusmb1995/llama.cpp

src/llama-model-load-input.cpp
@@ -1,0 +1,23 @@
+#include "llama-model-load-input.h"
+
+namespace load_input_variant {
+
+const char * identifier(load_input_t & load_input) {
+    if (std::holds_alternative<llama_fname_load_input>(load_input)) {
+        const auto & file_input = std::get<llama_fname_load_input>(load_input);
+        return file_input.fname.c_str();
+    }
+    static const char * buffer_id_str = "buffer";
+    return buffer_id_str;
+}
+
+fname_load_input split_name_from_variant(load_input_t & load_input) {
+    auto file_input = std::get<llama_fname_load_input>(load_input);
+    return file_input;
+}
+
+bool variant_supports_split_load(load_input_t & load_input) {
+    return std::holds_alternative<llama_fname_load_input>(load_input);
+}
+
+}  // namespace load_input_variant

src/llama-model-load.cpp
@@ -1,0 +1,30 @@
+#include "llama-model-load.h"
+
+#include "llama-model-loader.h"
+
+#include <memory>
+#include <stdexcept>
+#include <variant>
+
+llama_gguf_file_load::llama_gguf_file_load(struct ggml_context ** ctx, load_input_t load_input) :
+    params({
+        /*.no_alloc = */ true,
+        /*.ctx      = */ ctx,
+    }) {
+    using namespace load_input_variant;
+    if (std::holds_alternative<llama_fname_load_input>(load_input)) {
+        const auto & file_input = std::get<llama_fname_load_input>(load_input);
+        meta.reset(gguf_init_from_file(file_input.fname.c_str(), params));
+        if (!meta) {
+            throw std::runtime_error(format("%s: failed to load model from %s", __func__, file_input.fname.c_str()));
+        }
+        file = std::make_unique<llama_file_disk>(file_input.fname.c_str(), "ro");
+    } else {
+        const auto & buffer_input = std::get<llama_buffer_load_input>(load_input);
+        meta.reset(gguf_init_from_buffer(*buffer_input.streambuf, params));
+        if (!meta) {
+            throw std::runtime_error(format("%s: failed to load model from buffer", __func__));
+        }
+        file = std::make_unique<llama_file_buffer_ro>(std::move(buffer_input.streambuf));
+    }
+}