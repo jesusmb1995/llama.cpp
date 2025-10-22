tetherto/qvac-ext-lib-llama.cpp

[refactor] Process file method
Move the loader code, that process a file after it has been loaded into memory and populate its own attributes, to a reusable method.

src/llama-model-loader.h
@@ -5,6 +5,7 @@
 #include "llama-impl.h"
 #include "llama-arch.h"
 #include "llama-mmap.h"
+#include "llama-model-load.h"
 
 #include "ggml-cpp.h"
 
@@ -90,6 +91,8 @@ struct llama_model_loader {
     size_t size_done = 0;
     size_t size_data = 0;
     std::vector<std::pair<size_t, size_t>> mmaps_used;
+
+    void process_loaded_gguf(struct ggml_context * ctx, gguf_file_load & gguf_load, uint16_t idx);
 
     llama_model_loader(
         const std::string & fname,

jesusmb1995/llama.cpp

src/llama-model-loader.h
@@ -5,6 +5,7 @@
 #include "llama-impl.h"
 #include "llama-arch.h"
 #include "llama-mmap.h"
+#include "llama-model-load.h"
 
 #include "ggml-cpp.h"
 
@@ -91,6 +92,8 @@ struct llama_model_loader {
     size_t size_done = 0;
     size_t size_data = 0;
     std::vector<std::pair<size_t, size_t>> mmaps_used;
+
+    void process_loaded_gguf(struct ggml_context * ctx, llama_gguf_file_load & gguf_load, uint16_t idx);
 
     llama_model_loader(
         const std::string & fname,